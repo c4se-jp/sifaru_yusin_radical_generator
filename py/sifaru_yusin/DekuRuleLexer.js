7c1fd205cedef17d429a37a2c9b370a6844ee732
さっちゃん <ne-sachirou@hatena.ne.jp>
2026-06-21 21:08:38 +0900

    s/戻/戾/

--- py/sifaru_yusin/DekuRuleLexer.js
+++ py/sifaru_yusin/DekuRuleLexer.js
@@ -1,92 +1,130 @@
 // Generated from DekuRule.g4 by ANTLR 4.9
 // jshint ignore: start
-import antlr4 from 'antlr4';
+import antlr4 from "antlr4";
 
-
-
-const serializedATN = ["\u0003\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786",
-    "\u5964\u0002\u000fT\b\u0001\u0004\u0002\t\u0002\u0004\u0003\t\u0003",
-    "\u0004\u0004\t\u0004\u0004\u0005\t\u0005\u0004\u0006\t\u0006\u0004\u0007",
-    "\t\u0007\u0004\b\t\b\u0004\t\t\t\u0004\n\t\n\u0004\u000b\t\u000b\u0004",
-    "\f\t\f\u0004\r\t\r\u0004\u000e\t\u000e\u0003\u0002\u0003\u0002\u0003",
-    "\u0003\u0003\u0003\u0003\u0004\u0003\u0004\u0003\u0005\u0003\u0005\u0003",
-    "\u0006\u0003\u0006\u0003\u0007\u0003\u0007\u0003\b\u0003\b\u0003\t\u0003",
-    "\t\u0003\n\u0003\n\u0003\n\u0007\n1\n\n\f\n\u000e\n4\u000b\n\u0005\n",
-    "6\n\n\u0003\u000b\u0003\u000b\u0003\u000b\u0003\u000b\u0003\f\u0003",
-    "\f\u0003\f\u0003\f\u0007\f@\n\f\f\f\u000e\fC\u000b\f\u0003\f\u0003\f",
-    "\u0003\r\u0006\rH\n\r\r\r\u000e\rI\u0003\r\u0003\r\u0003\u000e\u0003",
-    "\u000e\u0007\u000eP\n\u000e\f\u000e\u000e\u000eS\u000b\u000e\u0002\u0002",
-    "\u000f\u0003\u0003\u0005\u0004\u0007\u0005\t\u0006\u000b\u0007\r\b\u000f",
-    "\t\u0011\n\u0013\u000b\u0015\f\u0017\r\u0019\u000e\u001b\u000f\u0003",
-    "\u0002\b\u0003\u00023;\u0003\u00022;\u0003\u0002$$\u0005\u0002\u000b",
-    "\f\u000f\u000f\"\"\u0003\u0002C\\\u0005\u00022;C\\c|\u0002Y\u0002\u0003",
-    "\u0003\u0002\u0002\u0002\u0002\u0005\u0003\u0002\u0002\u0002\u0002\u0007",
-    "\u0003\u0002\u0002\u0002\u0002\t\u0003\u0002\u0002\u0002\u0002\u000b",
-    "\u0003\u0002\u0002\u0002\u0002\r\u0003\u0002\u0002\u0002\u0002\u000f",
-    "\u0003\u0002\u0002\u0002\u0002\u0011\u0003\u0002\u0002\u0002\u0002\u0013",
-    "\u0003\u0002\u0002\u0002\u0002\u0015\u0003\u0002\u0002\u0002\u0002\u0017",
-    "\u0003\u0002\u0002\u0002\u0002\u0019\u0003\u0002\u0002\u0002\u0002\u001b",
-    "\u0003\u0002\u0002\u0002\u0003\u001d\u0003\u0002\u0002\u0002\u0005\u001f",
-    "\u0003\u0002\u0002\u0002\u0007!\u0003\u0002\u0002\u0002\t#\u0003\u0002",
-    "\u0002\u0002\u000b%\u0003\u0002\u0002\u0002\r\'\u0003\u0002\u0002\u0002",
-    "\u000f)\u0003\u0002\u0002\u0002\u0011+\u0003\u0002\u0002\u0002\u0013",
-    "5\u0003\u0002\u0002\u0002\u00157\u0003\u0002\u0002\u0002\u0017;\u0003",
-    "\u0002\u0002\u0002\u0019G\u0003\u0002\u0002\u0002\u001bM\u0003\u0002",
-    "\u0002\u0002\u001d\u001e\u0007+\u0002\u0002\u001e\u0004\u0003\u0002",
-    "\u0002\u0002\u001f \u0007\u007f\u0002\u0002 \u0006\u0003\u0002\u0002",
-    "\u0002!\"\u0007?\u0002\u0002\"\b\u0003\u0002\u0002\u0002#$\u0007=\u0002",
-    "\u0002$\n\u0003\u0002\u0002\u0002%&\u0007/\u0002\u0002&\f\u0003\u0002",
-    "\u0002\u0002\'(\u0007*\u0002\u0002(\u000e\u0003\u0002\u0002\u0002)*",
-    "\u0007}\u0002\u0002*\u0010\u0003\u0002\u0002\u0002+,\u0007~\u0002\u0002",
-    ",\u0012\u0003\u0002\u0002\u0002-6\u00072\u0002\u0002.2\t\u0002\u0002",
-    "\u0002/1\t\u0003\u0002\u00020/\u0003\u0002\u0002\u000214\u0003\u0002",
-    "\u0002\u000220\u0003\u0002\u0002\u000223\u0003\u0002\u0002\u000236\u0003",
-    "\u0002\u0002\u000242\u0003\u0002\u0002\u00025-\u0003\u0002\u0002\u0002",
-    "5.\u0003\u0002\u0002\u00026\u0014\u0003\u0002\u0002\u000278\u0007t\u0002",
-    "\u000289\u0007g\u0002\u00029:\u0007h\u0002\u0002:\u0016\u0003\u0002",
-    "\u0002\u0002;A\u0007$\u0002\u0002<=\u0007^\u0002\u0002=@\u0007$\u0002",
-    "\u0002>@\n\u0004\u0002\u0002?<\u0003\u0002\u0002\u0002?>\u0003\u0002",
-    "\u0002\u0002@C\u0003\u0002\u0002\u0002A?\u0003\u0002\u0002\u0002AB\u0003",
-    "\u0002\u0002\u0002BD\u0003\u0002\u0002\u0002CA\u0003\u0002\u0002\u0002",
-    "DE\u0007$\u0002\u0002E\u0018\u0003\u0002\u0002\u0002FH\t\u0005\u0002",
-    "\u0002GF\u0003\u0002\u0002\u0002HI\u0003\u0002\u0002\u0002IG\u0003\u0002",
-    "\u0002\u0002IJ\u0003\u0002\u0002\u0002JK\u0003\u0002\u0002\u0002KL\b",
-    "\r\u0002\u0002L\u001a\u0003\u0002\u0002\u0002MQ\t\u0006\u0002\u0002",
-    "NP\t\u0007\u0002\u0002ON\u0003\u0002\u0002\u0002PS\u0003\u0002\u0002",
-    "\u0002QO\u0003\u0002\u0002\u0002QR\u0003\u0002\u0002\u0002R\u001c\u0003",
-    "\u0002\u0002\u0002SQ\u0003\u0002\u0002\u0002\t\u000225?AIQ\u0003\b\u0002",
-    "\u0002"].join("");
-
+const serializedATN = [
+  "\u0003\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786",
+  "\u5964\u0002\u000fT\b\u0001\u0004\u0002\t\u0002\u0004\u0003\t\u0003",
+  "\u0004\u0004\t\u0004\u0004\u0005\t\u0005\u0004\u0006\t\u0006\u0004\u0007",
+  "\t\u0007\u0004\b\t\b\u0004\t\t\t\u0004\n\t\n\u0004\u000b\t\u000b\u0004",
+  "\f\t\f\u0004\r\t\r\u0004\u000e\t\u000e\u0003\u0002\u0003\u0002\u0003",
+  "\u0003\u0003\u0003\u0003\u0004\u0003\u0004\u0003\u0005\u0003\u0005\u0003",
+  "\u0006\u0003\u0006\u0003\u0007\u0003\u0007\u0003\b\u0003\b\u0003\t\u0003",
+  "\t\u0003\n\u0003\n\u0003\n\u0007\n1\n\n\f\n\u000e\n4\u000b\n\u0005\n",
+  "6\n\n\u0003\u000b\u0003\u000b\u0003\u000b\u0003\u000b\u0003\f\u0003",
+  "\f\u0003\f\u0003\f\u0007\f@\n\f\f\f\u000e\fC\u000b\f\u0003\f\u0003\f",
+  "\u0003\r\u0006\rH\n\r\r\r\u000e\rI\u0003\r\u0003\r\u0003\u000e\u0003",
+  "\u000e\u0007\u000eP\n\u000e\f\u000e\u000e\u000eS\u000b\u000e\u0002\u0002",
+  "\u000f\u0003\u0003\u0005\u0004\u0007\u0005\t\u0006\u000b\u0007\r\b\u000f",
+  "\t\u0011\n\u0013\u000b\u0015\f\u0017\r\u0019\u000e\u001b\u000f\u0003",
+  "\u0002\b\u0003\u00023;\u0003\u00022;\u0003\u0002$$\u0005\u0002\u000b",
+  '\f\u000f\u000f""\u0003\u0002C\\\u0005\u00022;C\\c|\u0002Y\u0002\u0003',
+  "\u0003\u0002\u0002\u0002\u0002\u0005\u0003\u0002\u0002\u0002\u0002\u0007",
+  "\u0003\u0002\u0002\u0002\u0002\t\u0003\u0002\u0002\u0002\u0002\u000b",
+  "\u0003\u0002\u0002\u0002\u0002\r\u0003\u0002\u0002\u0002\u0002\u000f",
+  "\u0003\u0002\u0002\u0002\u0002\u0011\u0003\u0002\u0002\u0002\u0002\u0013",
+  "\u0003\u0002\u0002\u0002\u0002\u0015\u0003\u0002\u0002\u0002\u0002\u0017",
+  "\u0003\u0002\u0002\u0002\u0002\u0019\u0003\u0002\u0002\u0002\u0002\u001b",
+  "\u0003\u0002\u0002\u0002\u0003\u001d\u0003\u0002\u0002\u0002\u0005\u001f",
+  "\u0003\u0002\u0002\u0002\u0007!\u0003\u0002\u0002\u0002\t#\u0003\u0002",
+  "\u0002\u0002\u000b%\u0003\u0002\u0002\u0002\r\'\u0003\u0002\u0002\u0002",
+  "\u000f)\u0003\u0002\u0002\u0002\u0011+\u0003\u0002\u0002\u0002\u0013",
+  "5\u0003\u0002\u0002\u0002\u00157\u0003\u0002\u0002\u0002\u0017;\u0003",
+  "\u0002\u0002\u0002\u0019G\u0003\u0002\u0002\u0002\u001bM\u0003\u0002",
+  "\u0002\u0002\u001d\u001e\u0007+\u0002\u0002\u001e\u0004\u0003\u0002",
+  "\u0002\u0002\u001f \u0007\u007f\u0002\u0002 \u0006\u0003\u0002\u0002",
+  '\u0002!"\u0007?\u0002\u0002"\b\u0003\u0002\u0002\u0002#$\u0007=\u0002',
+  "\u0002$\n\u0003\u0002\u0002\u0002%&\u0007/\u0002\u0002&\f\u0003\u0002",
+  "\u0002\u0002\'(\u0007*\u0002\u0002(\u000e\u0003\u0002\u0002\u0002)*",
+  "\u0007}\u0002\u0002*\u0010\u0003\u0002\u0002\u0002+,\u0007~\u0002\u0002",
+  ",\u0012\u0003\u0002\u0002\u0002-6\u00072\u0002\u0002.2\t\u0002\u0002",
+  "\u0002/1\t\u0003\u0002\u00020/\u0003\u0002\u0002\u000214\u0003\u0002",
+  "\u0002\u000220\u0003\u0002\u0002\u000223\u0003\u0002\u0002\u000236\u0003",
+  "\u0002\u0002\u000242\u0003\u0002\u0002\u00025-\u0003\u0002\u0002\u0002",
+  "5.\u0003\u0002\u0002\u00026\u0014\u0003\u0002\u0002\u000278\u0007t\u0002",
+  "\u000289\u0007g\u0002\u00029:\u0007h\u0002\u0002:\u0016\u0003\u0002",
+  "\u0002\u0002;A\u0007$\u0002\u0002<=\u0007^\u0002\u0002=@\u0007$\u0002",
+  "\u0002>@\n\u0004\u0002\u0002?<\u0003\u0002\u0002\u0002?>\u0003\u0002",
+  "\u0002\u0002@C\u0003\u0002\u0002\u0002A?\u0003\u0002\u0002\u0002AB\u0003",
+  "\u0002\u0002\u0002BD\u0003\u0002\u0002\u0002CA\u0003\u0002\u0002\u0002",
+  "DE\u0007$\u0002\u0002E\u0018\u0003\u0002\u0002\u0002FH\t\u0005\u0002",
+  "\u0002GF\u0003\u0002\u0002\u0002HI\u0003\u0002\u0002\u0002IG\u0003\u0002",
+  "\u0002\u0002IJ\u0003\u0002\u0002\u0002JK\u0003\u0002\u0002\u0002KL\b",
+  "\r\u0002\u0002L\u001a\u0003\u0002\u0002\u0002MQ\t\u0006\u0002\u0002",
+  "NP\t\u0007\u0002\u0002ON\u0003\u0002\u0002\u0002PS\u0003\u0002\u0002",
+  "\u0002QO\u0003\u0002\u0002\u0002QR\u0003\u0002\u0002\u0002R\u001c\u0003",
+  "\u0002\u0002\u0002SQ\u0003\u0002\u0002\u0002\t\u000225?AIQ\u0003\b\u0002",
+  "\u0002",
+].join("");
 
 const atn = new antlr4.atn.ATNDeserializer().deserialize(serializedATN);
 
-const decisionsToDFA = atn.decisionToState.map( (ds, index) => new antlr4.dfa.DFA(ds, index) );
+const decisionsToDFA = atn.decisionToState.map(
+  (ds, index) => new antlr4.dfa.DFA(ds, index),
+);
 
 export default class DekuRuleLexer extends antlr4.Lexer {
+  static grammarFileName = "DekuRule.g4";
+  static channelNames = ["DEFAULT_TOKEN_CHANNEL", "HIDDEN"];
+  static modeNames = ["DEFAULT_MODE"];
+  static literalNames = [
+    null,
+    "')'",
+    "'}'",
+    "'='",
+    "';'",
+    "'-'",
+    "'('",
+    "'{'",
+    "'|'",
+    null,
+    "'ref'",
+  ];
+  static symbolicNames = [
+    null,
+    "CloseArguments",
+    "CloseParenthesis",
+    "DefineOperator",
+    "DefinitionTerminator",
+    "DifferenceOperator",
+    "OpenArguments",
+    "OpenParenthesis",
+    "OrOperator",
+    "PositiveInteger",
+    "ReferenceCall",
+    "String",
+    "WhiteSpace",
+    "WordName",
+  ];
+  static ruleNames = [
+    "CloseArguments",
+    "CloseParenthesis",
+    "DefineOperator",
+    "DefinitionTerminator",
+    "DifferenceOperator",
+    "OpenArguments",
+    "OpenParenthesis",
+    "OrOperator",
+    "PositiveInteger",
+    "ReferenceCall",
+    "String",
+    "WhiteSpace",
+    "WordName",
+  ];
 
-    static grammarFileName = "DekuRule.g4";
-    static channelNames = [ "DEFAULT_TOKEN_CHANNEL", "HIDDEN" ];
-	static modeNames = [ "DEFAULT_MODE" ];
-	static literalNames = [ null, "')'", "'}'", "'='", "';'", "'-'", "'('", 
-                         "'{'", "'|'", null, "'ref'" ];
-	static symbolicNames = [ null, "CloseArguments", "CloseParenthesis", "DefineOperator", 
-                          "DefinitionTerminator", "DifferenceOperator", 
-                          "OpenArguments", "OpenParenthesis", "OrOperator", 
-                          "PositiveInteger", "ReferenceCall", "String", 
-                          "WhiteSpace", "WordName" ];
-	static ruleNames = [ "CloseArguments", "CloseParenthesis", "DefineOperator", 
-                      "DefinitionTerminator", "DifferenceOperator", "OpenArguments", 
-                      "OpenParenthesis", "OrOperator", "PositiveInteger", 
-                      "ReferenceCall", "String", "WhiteSpace", "WordName" ];
+  constructor(input) {
+    super(input);
+    this._interp = new antlr4.atn.LexerATNSimulator(
+      this,
+      atn,
+      decisionsToDFA,
+      new antlr4.PredictionContextCache(),
+    );
+  }
 
-    constructor(input) {
-        super(input)
-        this._interp = new antlr4.atn.LexerATNSimulator(this, atn, decisionsToDFA, new antlr4.PredictionContextCache());
-    }
-
-    get atn() {
-        return atn;
-    }
+  get atn() {
+    return atn;
+  }
 }
 
 DekuRuleLexer.EOF = antlr4.Token.EOF;
@@ -103,6 +141,3 @@ DekuRuleLexer.ReferenceCall = 10;
 DekuRuleLexer.String = 11;
 DekuRuleLexer.WhiteSpace = 12;
 DekuRuleLexer.WordName = 13;
-
-
-
